8876a73f9752978ef2a9a9589398ade32f9b7c8b
Swarna Jang Maden <limbujungmaden@gmail.com>
2024-06-25 22:19:01 +0545

    add readme file

--- src/utils/getRandomInt.ts
+++
@@ -1,5 +0,0 @@
-export function getRandomInt(min: number, max: number): number {
-     const minCeiled = Math.ceil(min);
-     const maxFloored = Math.floor(max);
-     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-}
